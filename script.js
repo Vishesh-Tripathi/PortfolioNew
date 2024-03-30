779a8ac89e7e2dcbfe28f7de18fd00c3bc604e90
VISHESH TRIPATHI <tripathiishu467@gmail.com>
2024-03-30 19:14:36 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+var typed = new Typed(".multiple_text", {
+    strings:[
+        "Frontend Developer",
+        "3-D Designer",
+        "YouTuber"],
+    typeSpeed: 100,
+    backSpeed: 100,
+    backDelay: 1000,
+    loop: true
+})
+
