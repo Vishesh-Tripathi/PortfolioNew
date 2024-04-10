addd215647c300ba25f91a9d9aeabf7c1e66011e
VISHESH TRIPATHI <tripathiishu467@gmail.com>
2024-04-11 00:12:07 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 var typed = new Typed(".multiple_text", {
     strings:[
         "Frontend Developer",
-        "3-D Designer",
-        "YouTuber"],
+        
+    ],
     typeSpeed: 100,
     backSpeed: 100,
     backDelay: 1000,
